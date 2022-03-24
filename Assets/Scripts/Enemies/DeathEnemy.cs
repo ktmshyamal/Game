e7127a42224672d89d94c60ccf650dc7cb0a345b
Gayan Shyamal <ktmshyamal@gmail.com>
2022-03-24 12:25:24 +0530

    Enemy correct position change & attack

--- Assets/Scripts/Enemies/DeathEnemy.cs
+++ Assets/Scripts/Enemies/DeathEnemy.cs
@@ -45,7 +45,8 @@ public class DeathEnemy : MonoBehaviour
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+        RaycastHit2D hit =
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0, Vector2.left, 0, playerLayer);
 
