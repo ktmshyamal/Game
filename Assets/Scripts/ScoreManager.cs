24a9e1ffebaa1773ccafa96029d013989fe85c59
Gayan Shyamal <ktmshyamal@gmail.com>
2022-03-27 16:04:03 +0530

    UI design

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public Text scoreText;
+    public Text hignscoreText;
+
+    int score = 0;
+    int highscore = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+        scoreText.text = score.ToString() + " POINTS";
+        hignscoreText.text = "HIGHSCORE: " + highscore.ToString();
+    }
+
+    public void AddPoint()
+    {
+        score += 1;
+        scoreText.text = score.ToString() + "POINTS";
+        if (highscore < score)
+            PlayerPrefs.SetFloat("highscore", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
